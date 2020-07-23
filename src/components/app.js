cfd2c5eee650ff21f0f53836edaf4585f4fe2c74
hollieherbel <herbel6521@gmail.com>
2020-07-23 15:49:17 -0500

    fixed heroku app links

--- src/components/app.js
+++ src/components/app.js
@@ -31,7 +31,7 @@ export default class App extends Component {
 
 
     getListings() {
-        fetch("https://olivetree-backend.herokuapp.com/listings", { method: "GET" })
+        fetch("https://olivetree-backend.herokuapp.com/listing/get", { method: "GET" })
         .then(response => response.json())
         .then(data => this.setState({ listingList: data }))
         .catch(error => console.log(error))
